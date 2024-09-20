e63965eddde86260e97a1be49ea7aea31757c50f
VigneshNaveen1311 <svigneshnaveen1311@gmail.com>
2024-09-20 14:45:07 +0530

    added image upload functionality

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template,request,jsonify
+from flask import Flask, render_template,request,jsonify, url_for,redirect
 from bayes_fn import bayes_fn
+import os
 app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
 @app.route('/')
 def index():
     return "hi bro from python<br>" + render_template('index.html')
@@ -8,6 +15,29 @@ def index():
 @app.route('/bayes_page')
 def bayes_page():
     return render_template('bayes.html')
+
+@app.route('/product_finder_page')
+def product_finder_page():
+    return render_template('productfinder.html')
+
+@app.route('/productfind', methods = ['POST'])
+def product_find():
+    if 'image' not in request.files:
+        return 'No file part'
+    
+    file  = request.files['image']
+
+    if file.filename == '':
+        return 'No selected file'
+    
+    if file:
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'],file.filename)
+        file.save(file_path)
+        return redirect(url_for('display_image',filename = file.filename))
+    
+@app.route('/uploads/<filename>')
+def display_image(filename):
+    return render_template('display_image.html', filename=filename)
 
 @app.route('/calculate_bayes',methods = ['POST'])
 def calculate_bayes():
